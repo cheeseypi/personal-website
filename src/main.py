e4513824bdda7c1c177db6490d6d976cccb1eb40
Matthew Doto <mvd369@gmail.com>
2018-06-10 03:17:52 -0400

    Working SASS + makefile

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import json
 
 from jinja2 import FileSystemLoader, Environment
 
+COMPILE_LOCATION = "../docs/"
+
 templateLoader = FileSystemLoader(searchpath="./templates")
 templateEnv = Environment(loader=templateLoader)
 template = templateEnv.get_template("base.html")
@@ -12,8 +14,6 @@ with open("content.json", "r") as cont:
 links = info['links']
 
 content = info['content']
-
-COMPILE_LOCATION = "../docs/"
 for page, details in content.items():
     with open(COMPILE_LOCATION+page+".html", "w+") as newPage:
         newPage.write(template.render(links=links, content=details))
